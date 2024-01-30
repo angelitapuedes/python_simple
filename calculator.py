4ef74223c0e25e96858101326fc761cd0e5cff75
angelarodriguez <angiemarirodriguez@gmail.com>
2024-01-30 09:05:00 -0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+def add(num1, num2):
+    return num1 + num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+if __name__ == "__main__":  
+    print(add(6,9))
+    print(divide(6,9))
+    print(multiply(6,9))
+    
